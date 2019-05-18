ea333420ab5db29316dee30a8e1a9c80fce298f2
Tony Baker <this.tonybaker@gmail.com>
2019-05-18 00:03:44 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO users(name, age) VALUES ('Marcell', 132);
+INSERT INTO users(name, age) VALUES('Jak', 12);
+INSERT INTO users(name, age) VALUES('Rebekah', 993);
+INSERT INTO users(name, age) VALUES('Kol', 991);
+INSERT INTO users(name, age) VALUES('Ben', 56);
+INSERT INTO users(name, age) VALUES('Hope', 1);
+INSERT INTO users(name, age) VALUES('Tony', 44);
+INSERT INTO users(name, age) VALUES('Darin', 30);
+INSERT INTO users(name, age) VALUES('David', 23);
+INSERT INTO users(name, age) VALUES('Kat', 34);
+INSERT INTO users(name, age) VALUES ('Marcell', 132);
+INSERT INTO users(name, age) VALUES('Jak', 12);
+INSERT INTO users(name, age) VALUES('Rebekah', 993);
+INSERT INTO users(name, age) VALUES('Kol', 991);
+INSERT INTO users(name, age) VALUES('Ben', 56);
+INSERT INTO users(name, age) VALUES('Hope', 1);
+INSERT INTO users(name, age) VALUES('Tony', 44);
+INSERT INTO users(name, age) VALUES('Darin', 30);
+INSERT INTO users(name, age) VALUES('David', 23);
+INSERT INTO users(name, age) VALUES('Kat', 34);
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+  VALUES('Happy Birthday', 'Celebration', 200, 'Febuary 28th', 'Febuary 29th');
+INSERT INTO pledges(amount, user_id, project_id)
+  VALUES(1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45),
+  (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45),
+  (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45),
+  (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45),
+  (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45),
+  (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45), (1000, 1, 45);
